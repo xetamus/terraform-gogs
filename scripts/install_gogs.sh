e98c8dec0528124e064bc05d719df7c36aefc411
Ubuntu <ubuntu@tux01>
2017-02-17 17:50:20 +0000

    Remove unused scripts directory

--- scripts/install_gogs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-wget -qO - https://deb.packager.io/key | sudo apt-key add -
-echo "deb https://deb.packager.io/gh/pkgr/gogs xenial pkgr" | sudo tee /etc/apt/sources.list.d/gogs.list
-apt-get update -y
-apt-get install -y gogs
